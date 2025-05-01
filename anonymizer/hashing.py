92adab89b195ab5edeebc3f1abfa46f08a1e541d
Shreyas Bargale <134770064+asdf1879@users.noreply.github.com>
2025-05-01 10:58:47 +0530

    Delete anonymizer directory

--- anonymizer/hashing.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-import os
-
-SALT = os.urandom(16)  # Generate a unique salt for this session
-
-def hash_with_salt(value):
-    """Hashes a value using SHA-256 with a salt."""
-    salted_value = SALT + value.encode()
-    return hashlib.sha256(salted_value).hexdigest()
